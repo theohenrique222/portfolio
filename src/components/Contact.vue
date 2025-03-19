df32efece76fd367499d876c9b91a7bf001ca50c
Theo Henrique <theodoro222@hotmail.com>
2025-03-19 20:08:07 -0300

    alteração em links de contate-me

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,11 +1,7 @@
 <template>
   <section class="bg-neutral-900" id="contato">
-    <main
-      class="grid lg:px-0 px-10 lg:grid-cols-2 items-center gap-6 py-10 max-w-6xl m-auto"
-      id="contact"
-      data-aos="fade-up"
-      data-aos-duration="3000"
-    >
+    <main class="grid lg:px-0 px-10 lg:grid-cols-2 items-center gap-6 py-10 max-w-6xl m-auto" id="contact"
+      data-aos="fade-up" data-aos-duration="3000">
       <div class="contact-text">
         <h2 class="font-extrabold text-white text-4xl md:text-5xl">
           Contate-<span class="text-orange-color">Me!</span>
@@ -18,60 +14,33 @@
 
         <div class="mb-7 text-white">
           <li class="mb-3 list-none ">
-            <a  
-              v-for="social in social_icons" :key="social.id"
+            <a v-for="social in social_icons" :key="social.id"
               class="transition-all duration-300 hover:translate-x-2 items-center flex text-base hover:text-orange-color"
-              href="https://wa.me/message/TQG43CPOB4N2N1"
-              target="_blank"
-              ><ion-icon :name=social.name class="px-3"></ion-icon>{{ social.content }}</a
-            >
+              :href="social.href" target="_blank"><ion-icon :name=social.name class="px-3"></ion-icon>{{ social.content
+              }}</a>
           </li>
         </div>
       </div>
       <div class="contact-form">
-        <form
-          action="https://formsubmit.co/theodoro222@hotmail.com"
-          method="POST"
-          enctype="text/plain"
-        >
-          <div
-            v-for="form in form_inputs"
-            :key="form.id"
-            class="relative mb-3 space-y-1"
-          >
-            <label
-              :for="form.title"
-              class="relative left-0 top-0 ease-out text-white"
-              >{{ form.title }}
+        <form action="https://formsubmit.co/theodoro222@hotmail.com" method="POST" enctype="text/plain">
+          <div v-for="form in form_inputs" :key="form.id" class="relative mb-3 space-y-1">
+            <label :for="form.title" class="relative left-0 top-0 ease-out text-white">{{ form.title }}
             </label>
-            <input
-              :id="form.id_input"
-              :name="form.name"
-              :type="form.type"
-              required
+            <input :id="form.id_input" :name="form.name" :type="form.type" required
               class="block w-full rounded bg-neutral-800 text-white p-3 outline-none "
-              :placeholder="form.placeholder"
-            />
+              :placeholder="form.placeholder" />
           </div>
 
           <div class="relative mb-3 space-y-1">
-            <label
-              for="mensagem"
-              class="relative left-0 top-0 ease-out text-white "
-              >Em que posso te ajudar?
-          </label>
-            <textarea
-              name="mensagem: "
-              id="textarea"
-              rows="3"
-              class="h-60 block w-full rounded resize-none bg-neutral-800 text-white p-3"
-            ></textarea>
+            <label for="mensagem" class="relative left-0 top-0 ease-out text-white ">Em que posso te ajudar?
+            </label>
+            <textarea name="mensagem: " id="textarea" rows="3"
+              class="h-60 block w-full rounded resize-none bg-neutral-800 text-white p-3"></textarea>
           </div>
 
           <button
             class="inline-block w-full text-white bg-success-500 px-10 p-4 rounded-md hover:bg-success-800 tracking-wider"
-            type="submit"
-          >
+            type="submit">
             Enviar Mensagem
           </button>
         </form>
@@ -111,8 +80,9 @@ export default {
         },
       ],
       social_icons: [
-        { id:1, content: '+55 (84) 99127-4081', name: 'logo-whatsapp' },
-        { id:2, content: 'theodoro222@hotmail.com', name: 'mail-outline' },
+        { id: 1, content: 'Theodoro Henrique', name: 'logo-linkedin', href: 'https://www.linkedin.com/in/theodoro-henrique-20458827a/' },
+        { id: 2, content: 'theodoro222@hotmail.com', name: 'mail-outline', href: 'mailto:theodoro222@hotmail.com' },
+        { id: 3, content: '@theohbezerra_', name: 'logo-instagram', href: 'https://www.instagram.com/theohbezerra_/?hl=pt' },
       ],
     };
   },
@@ -122,6 +92,7 @@ export default {
 input[type="number"]::-webkit-inner-spin-button {
   -webkit-appearance: none;
 }
+
 input[type="number"] {
   -moz-appearance: textfield;
   appearance: textfield;
