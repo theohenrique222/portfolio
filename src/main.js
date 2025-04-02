de482aef91fd7042affec914a485eeb6c49f7542
Theo Henrique <theodoro222@hotmail.com>
2025-04-02 19:39:24 -0300

    add internationalization with vue-i18n

--- src/main.js
+++ src/main.js
@@ -1,17 +1,26 @@
 import { createApp } from 'vue'
 import './style.css'
+import { createI18n } from 'vue-i18n';
 import App from './App.vue'
 
-
+import en from './locales/en.json';
+import pt from './locales/pt.json';
 
 import { Carousel, initTWE, Input } from "tw-elements";
 
-createApp(App).mount('#app')
+const i18n = createI18n({
+  locale: 'pt',
+  fallbackLocale: 'en',
+  messages: { en, pt }
+});
 
-export default {
-    mounted() {
-      initTWE({ Carousel, Input });
-    }
+const app = createApp(App);
+app.use(i18n);
+
+app.mixin({
+  mounted() {
+    initTWE({ Carousel, Input });
   }
+});
 
-
+app.mount('#app');
