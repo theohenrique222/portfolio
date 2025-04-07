3afd7a2c327a4bb997491a226f9638a0cb4a7e26
Theo Henrique <theodoro222@hotmail.com>
2025-04-06 23:34:52 -0300

    add animation and correction in cards

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -2,36 +2,27 @@
   <div class="bg-neutral-950 flex items-center justify-center" id="servicos">
     <div class="container mx-auto py-8 max-w-6xl lg:px-0 px-5">
       <div class="text-center text-white mb-20 mt-10">
-        <h3 class="font-extralight">{{ $t("services-intro") }}</h3>
-        <h1 class="font-extrabold text-4xl md:text-5xl">
+        <h3 data-aos="fade-up" data-aos-duration="2000" class="font-extralight">{{ $t("services-intro") }}</h3>
+        <h1 data-aos="fade-up" data-aos-duration="2000" class="font-extrabold text-4xl md:text-5xl">
           <span class="text-orange-color text-4xl md:text-5xl">{{ $t("services-title-1") }}</span>
           {{ $t("services-title-2") }}
         </h1>
       </div>
-      <div class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-8">
-        <div
-          v-for="card in cards_container"
-          :key="card.id"
-          class="bg-neutral-800 shadow-lg rounded-lg p-6 flex flex-col items-center space-y-5"
-        >
-          <ion-icon
-            :name="card.ionIcon"
-            class="text-5xl text-orange-500 mb-10 pt-5"
-          ></ion-icon>
-          <h2 class="text-2xl font-semibold text-white text-center mb-4">
+      <div data-aos="fade-up" data-aos-duration="2000" class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-8">
+        <div v-for="card in cards_container" :key="card.id"
+          class="bg-neutral-800 shadow-lg rounded-lg p-6 flex flex-col items-center space-y-5">
+          <ion-icon :name="card.ionIcon" class="text-5xl text-orange-500 mb-10 pt-5"></ion-icon>
+          <h2 class="text-xl font-semibold text-white text-center mb-4">
             {{ $t(card.title) }}
           </h2>
-          <h3 class="text-gray-100 text-center mb-4">{{ $t(card.content) }}</h3>
+          <h3 class="text-gray-100 mb-4">{{ $t(card.content) }}</h3>
           <ul class="text-white font-extralight text-left">
             <li v-for="(item, index) in card.li_contents" :key="index" class="mb-2">
-              <ion-icon
-                name="chevron-forward-outline"
-                class="mr-2 text-orange-color"
-              ></ion-icon>
+              <span class="text-orange-color font-extrabold">></span> 
               {{ $t(item) }}
             </li>
           </ul>
-          <h4 class="text-orange-color font-bold tracking-wider italic text-center">
+          <h4 class="text-orange-color font-light tracking-wider italic text-center">
             {{ $t(card.conclusion) }}
           </h4>
         </div>
@@ -39,7 +30,7 @@
     </div>
   </div>
 </template>
-  
+
 <script>
 export default {
   data() {
